665029aa309ca982ec85a5cbcc1c40cc2134fa26
morihisashi <hisashi.mori@humanage.co.jp>
2023-05-17 15:10:05 +0900

    ChatMessage作成

---
+++ src/components/chat/ChatMessage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ChatMessage.scss';
+import { Avatar } from '@mui/material';
+
+const ChatMessage = () => {
+    return (
+        <div className='message'>
+            <Avatar />
+            <div className='messageInfo'>
+                <h4>
+                    Shin Code
+                    <span className='messageTimestamp'>2023/05/17</span>
+                </h4>
+                <p>メッセージ本文</p>
+            </div>
+        </div>
+    )
+}
+
+export default ChatMessage
