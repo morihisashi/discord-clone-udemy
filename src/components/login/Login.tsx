03fa08a1cab0e3065e2a49658430c28665efab11
morihisashi <hisashi.mori@humanage.co.jp>
2023-05-26 17:28:18 +0900

    ログイン機能の実装

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import './Login.scss';
 import { Button } from '@mui/material';
+import { auth, provider } from '../../firebase';
+import { signInWithPopup } from 'firebase/auth';
 
 const Login = () => {
+    const signin = () => {
+        signInWithPopup(auth, provider)
+            .catch((error) => {
+                alert(error.message);
+            })
+    }
     return (
         <div className='login'>
             <div className='loginLogo'>
                 <img src='./discordIcon.png' alt='' />
             </div>
-            <Button>ログイン</Button>
+            <Button onClick={signin}>ログイン</Button>
         </div>
     )
 }
